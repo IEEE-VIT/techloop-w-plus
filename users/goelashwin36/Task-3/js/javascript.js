1d897abbe5cbc55c2bb320ca1203800a2186271d
Ashwin <ashwin.goel2017@vitstudent.ac.in>
2018-02-21 16:00:18 +0530

    Changes: css written in external file, NaN error handled

--- users/goelashwin36/Task-3/js/javascript.js
+++ users/goelashwin36/Task-3/js/javascript.js
@@ -2,9 +2,11 @@ function check(){
     var radioValue = 0;
     for (var i =1; i <=5; i++){
         selector = $("input[name = '" + i + "']:checked");
-        radioValue=radioValue+ Number(selector.val());
+        if(Number(selector.val())==1)
+        {
+          radioValue=radioValue+ Number(selector.val());
+        }
     }
     $("#p1").text("Your score is- "+radioValue+"/5");
-
 
 }
